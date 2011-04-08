66f9d50fd502ec72fa8c3afb3a03915c41bcb0d3
Tim Sharpe <tim@sharpe.id.au>
2011-04-08 13:20:35 +1000

    Lexify defines

--- puppet_lexer/__init__.py
+++ puppet_lexer/__init__.py
@@ -8,10 +8,11 @@ class PuppetLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r' .*\n', Text),
             (r'\s*#.*\n', Comment.Singleline),
             (r'(class)(\s*)(.*?)(\s*)(\{)', bygroups(Keyword.Declaration, Text, Name.Class, Text, Punctuation)),
+            (r'(define)(\s*)(.*?)(\()', bygroups(Keyword.Declaration, Text, Name.Class, Punctuation), 'argumentlist'),
             (r'(.*?)(\s*)(\{)(\s*)', bygroups(Name.Class, Text, Punctuation, Text), 'resource'),
+            (r'(include)(\s*)(.*?)(\n)', bygroups(Keyword.Namespace, Text, Name.Class, Text)),
             (r'.*\n', Text),
         ],
         'resource': [
@@ -23,7 +24,7 @@ class PuppetLexer(RegexLexer):
         'instance': [
             (r"(\s*)(\S+?)(\s*)(=>)(\s*)", bygroups(Text, Name.Attribute, Text, Operator, Text), 'value'),
             (r'(\,)', Punctuation),
-            (r'(;)', Punctuation, '#pop:2'),
+            (r'(;)', Punctuation, '#pop'),
         ],
         'value': [
             (r"([A-Z].+?)(\[)(\".+\"|'.+')(\])", bygroups(Name.Namespace, Punctuation, String, Punctuation), '#pop'),
@@ -31,5 +32,10 @@ class PuppetLexer(RegexLexer):
             (r"[^;,\"'\s]+", String, '#pop'),
             (r'".+?"', String, '#pop'),
             (r"'.+?'", String, '#pop'),
+        ],
+        'argumentlist': [
+            (r"(\$\S+)(\s*)(=)(\s*)(\".*?\"|'.*?'|\S+)(\,)?(\s*)", bygroups(Name.Variable, Text, Operator, Text, String, Punctuation, Text)),
+            (r'(\$\S+)(\,)?(\s*)', bygroups(Name.Variable, Punctuation, Text)),
+            (r'(\))(\s*)(\{)', bygroups(Punctuation, Text, Punctuation), '#pop'),
         ],
     }
