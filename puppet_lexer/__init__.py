faf9dadb42866ca573bb58f01e6f70757bd9b432
Tim Sharpe <tim@sharpe.id.au>
2011-04-08 14:57:10 +1000

    Get escape chars working in double quoted strings

--- puppet_lexer/__init__.py
+++ puppet_lexer/__init__.py
@@ -15,6 +15,7 @@ class PuppetLexer(RegexLexer):
             (r'(include)(\s*)(.*?)(\n)', bygroups(Keyword.Namespace, Text, Name.Class, Text)),
             (r'.*\n', Text),
         ],
+        # TODO: test \" in namevar
         'resource': [
             (r'(\s*)(".+?")(:)', bygroups(Text, String, Punctuation), 'instance'),
             (r"(\s*)('.+?')(:)", bygroups(Text, String, Punctuation), 'instance'),
@@ -30,12 +31,18 @@ class PuppetLexer(RegexLexer):
             (r"([A-Z].+?)(\[)(\".+\"|'.+')(\])", bygroups(Name.Namespace, Punctuation, String, Punctuation), '#pop'),
             (r'[0-9]+', Number, '#pop'),
             (r"[^;,\"'\s]+", String, '#pop'),
-            (r'".+?"', String, '#pop'),
+            (r'"', String, 'valdblstring'),
             (r"'.+?'", String, '#pop'),
         ],
+        # TODO: test \" in argument
         'argumentlist': [
             (r"(\$\S+)(\s*)(=)(\s*)(\".*?\"|'.*?'|\S+)(\,)?(\s*)", bygroups(Name.Variable, Text, Operator, Text, String, Punctuation, Text)),
             (r'(\$\S+)(\,)?(\s*)', bygroups(Name.Variable, Punctuation, Text)),
             (r'(\))(\s*)(\{)', bygroups(Punctuation, Text, Punctuation), '#pop'),
+        ],
+        'valdblstring': [
+            (r'(?:\\(?:[bdefnrstv\'"\\/]|[0-7][0-7]?[0-7]?|\^[a-zA-Z]))', String.Escape),
+            (r'[^"\\]+', String),
+            (r'"', String, '#pop:2'),
         ],
     }
